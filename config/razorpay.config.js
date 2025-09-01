148b15d46999db582fa2d3d7b1914fa151678aa0
Faisalyousufwani <165240262+Faisalyousufwani@users.noreply.github.com>
2025-09-01 18:42:52 +0530

    added  deliverable areas

---
+++ config/razorpay.config.js
@@ -0,0 +1,12 @@
+
+import Razorpay from "razorpay";
+import { configDotenv } from "dotenv";
+configDotenv('../.env')
+export const createRazorpayInstance=()=>{
+return new Razorpay(
+    {
+        key_id:process.env.RAZORPAY_KEY_ID,
+        key_secret:process.env.RAZORPAY_KEY_SECRET
+    }
+)
+}
